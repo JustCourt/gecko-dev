4c0d623c6a2dea193664ef039df15397725895c6
Courtney <courtney.j.siu@gmail.com>
2017-03-09 02:02:52 -0500

    Changed /browser/components/search/content/search.xml

--- toolkit/components/windowwatcher/test/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = { // eslint-disable-line no-undef
-  "extends": [
-    "../../../../testing/mochitest/browser.eslintrc.js",
-    "../../../../testing/mochitest/chrome.eslintrc.js",
-    "../../../../testing/mochitest/mochitest.eslintrc.js",
-  ]
-};
--- toolkit/mozapps/extensions/test/xpcshell/head_unpack.js
+++
@@ -1,4 +0,0 @@
-/* globals Services, TEST_UNPACKED: true */
-/* exported TEST_UNPACKED */
-Services.prefs.setBoolPref("extensions.alwaysUnpack", true);
-TEST_UNPACKED = true;
